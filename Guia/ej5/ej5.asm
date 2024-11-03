9bbfc23ceb71fa6a96dfa3947145befe99c66603
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-11-03 00:15:32 -0300

    Se fue mir

--- Guia/ej5/ej5.asm
+++ Guia/ej5/ej5.asm
@@ -18,10 +18,14 @@ STR_ARRAY_TOTAL_SIZE EQU 16
     mov rbp , rsp
 
     ; LO HACEMOS GUARDANDO VARIABLES LOCALES EN LA PILA 
-    sub rsp, 8 ; dejamos un espacio para una variable local en la pila
-    mov [rbp-8], rdi ; guardamos la capacidad en el espacio que dejamos
-
     
+    sub rsp, 8 ; dejamos un espacio para una variable local en la pila
+    mov [rbp-8], rdi ; guardamos la capacidad (argumento 1) en el espacio que dejamos
+    ; siempre en rbp-8 vamos a tener el valor del argumento 1 guardado (podemos acceder al arg1 desde esa direccion de la pila(stack))
+    
+    ;HASTA ACA LO UNICO QUE HICIMOS FUE GUARDAR EL REGISTRO(ARG CAPACITY) EN LA PILA
+
+    mov rdi, 
 
     mov rdi, 
 
