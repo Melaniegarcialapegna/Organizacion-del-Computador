9b12596fca711ef21022129cd1f4d88ef14240be
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2024-11-07 20:25:48 -0300

    falta terminar

--- Parciales/PrimerParcial1-C/1ASSEMBLY/ej1.asm
+++ Parciales/PrimerParcial1-C/1ASSEMBLY/ej1.asm
@@ -16,17 +16,52 @@ arrayMap:
 	
     push r12
     push r14
+    push r15
+    push r13
+    
 
     mov r12 ,rdi ; r12 : array_t
     mov r14, rsi ; r14 : funcion a aplicar
 
     mov rdi, ARRAY_T_TAM_TOTAL
     call malloc ;rax : puntero nuevoArr
-
-    aca no volatiles
+    
+    ;podemos usar volatiles siempre y cuando los terminemos de usar antes de hacer otro call
+    mov r15 ,rax ; r15 : nuevoArreglo
+    mov DWORD r8d, [r12] ; r8: array->type
+    mov BYTE cl, [r12+ARRAY_T_OFF_SIZE] ; r9 array->size
+    mov BYTE dl, [r12+ARRAY_T_OFF_CAPACITY] ;r10 array-> capacity
     
 
+    ;inicializamos valores del nuevoArr (linea 6-8)
+    mov DWORD [r15] , r8d
+    mov BYTE [r15+ARRAY_T_OFF_SIZE] , cl
+    mov BYTE [r15+ARRAY_T_OFF_CAPACITY] , dl
+    
+    ;multiplicacion
+    mov rax ,SIZE_VOID_ASTERISCO
+    mul [r12+ARRAY_T_OFF_SIZE]
+    ;rax : sizeof(void*)*(nuevoArr->capacity)
+    
+
+    ; le asignamos la data
+    mov rdi , rax
+    call malloc ; en rax tenemos la data
+    mov [r15+ARRAY_T_OFF_DATA], rax ; nuevoArr->data 
+
+    mov r13, 0 ; contador
+    
+    .while:
+        cmp r13 ,[r15+ARRAY_T_OFF_SIZE]
+        jl .finWhile
+        
+
+    .finWhile:
 
 
+    pop r13
+    pop r15
+    pop r14
+    pop r12
     pop rbp 
     ret	 
--- Parciales/PrimerParcial1-C/1ASSEMBLY/ej1.c
+++ Parciales/PrimerParcial1-C/1ASSEMBLY/ej1.c
@@ -3,9 +3,9 @@
 
 array_t* arrayMap(array_t* array, void* (*func)(void*)){    
     array_t* nuevoArr = malloc(sizeof(array_t));
+    nuevoArr->type = array->type;
     nuevoArr->size = array->size;
     nuevoArr->capacity = array->capacity;
-    nuevoArr->type = array->type;
     nuevoArr->data = malloc(sizeof(void*)*(nuevoArr->capacity)); //el malloc devuelve un puntero a la primera cajita dentro de la cajota(de tamanio capacidad) nuevoArr
     
     int contador = 0;
